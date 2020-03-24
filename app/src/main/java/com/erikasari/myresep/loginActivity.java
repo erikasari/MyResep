e7a0c79eccb413a167d9fcbebb19e803cb369358
erikasari <56011377+erikasari@users.noreply.github.com>
2020-03-24 10:40:37 +0700

    desain

---
+++ app/src/main/java/com/erikasari/myresep/loginActivity.java
@@ -0,0 +1,22 @@
+package com.erikasari.myresep;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class loginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public void handleReset(View view) {
+    }
+
+    public void handleLogin(View view) {
+    }
+
+}
+
